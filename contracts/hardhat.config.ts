61ea7a2cc2d84f6842005086bc65bf77c615e429
blockora <64456089+blockora@users.noreply.github.com>
2025-11-24 18:35:21 +0000

    ctrl+x

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,13 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
       version: "0.8.23",
           settings: {
-                optimizer: { enabled: true, runs: 200 }
-                    }
-                      }
-                      };
+                optimizer: { enabled: true, runs: 200 },
+                    },
+                      },
 
-                      export default config;
+                        networks: {
+                            blockora: {
+                                  url: process.env.RPC_URL || "",
+                                        accounts:
+                                                process.env.VALIDATOR_PRIVATE_KEY !== undefined
+                                                          ? [process.env.VALIDATOR_PRIVATE_KEY]
+                                                                    : [],
+                                                                        },
+                                                                          },
+                                                                          };
+
+                                                                          export default config;
