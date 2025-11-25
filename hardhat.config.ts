6c449704347026d8f897eeb0b7bda3b3d471d719
blockora <64456089+blockora@users.noreply.github.com>
2025-11-26 01:41:22 +0530

    Configure Hardhat with Solidity and network settings
    Signed-off-by: blockora <64456089+blockora@users.noreply.github.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,1 +1,26 @@
-hardhat.config.ts
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+      version: "0.8.23",
+          settings: {
+                optimizer: { enabled: true, runs: 200 },
+                    },
+                      },
+
+                        networks: {
+                            blockora: {
+                                  url: process.env.RPC_URL || "",
+                                        accounts:
+                                                process.env.VALIDATOR_PRIVATE_KEY !== undefined
+                                                          ? [process.env.VALIDATOR_PRIVATE_KEY]
+                                                                    : [],
+                                                                        },
+                                                                          },
+                                                                          };
+
+                                                                          export default config;
