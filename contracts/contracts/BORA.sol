c6c3a7c3702c3975c2298365f5b28b88c848a06b
blockora <64456089+blockora@users.noreply.github.com>
2025-11-30 08:18:10 +0000

    ctrl+s

--- contracts/contracts/BORA.sol
+++ contracts/contracts/BORA.sol
@@ -5,23 +5,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BORA is ERC20, Ownable {
-        // Total max supply: 100,000,000 BORA
             uint256 public constant MAX_SUPPLY = 100_000_000 ether;
 
                 constructor(
-                            address founder,
-                                    uint256 founderAmount,
-                                            address rewardsReceiver,
-                                                    uint256 rewardsAmount
+                                address founder,
+                                        uint256 founderAmount,
+                                                address rewardsReceiver,
+                                                        uint256 rewardsAmount
                 )
                         ERC20("BORA Token", "BORA")
-                                Ownable(founder)              // <<< YEH LINE ADD KARO
+                                Ownable(founder)
                                     {
                                                 require(founder != address(0), "founder is zero");
                                                         require(rewardsReceiver != address(0), "rewards receiver is zero");
                                                                 require(
-                                                                                founderAmount + rewardsAmount == MAX_SUPPLY,
-                                                                                            "exceeds max supply"
+                                                                                    founderAmount + rewardsAmount == MAX_SUPPLY,
+                                                                                                "exceeds max supply"
                                                                 );
 
                                                                         _mint(founder, founderAmount);
