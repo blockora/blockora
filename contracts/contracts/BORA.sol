c9c23b19faa42379cb0b025cfb53af374684b096
blockora <64456089+blockora@users.noreply.github.com>
2025-12-01 18:57:56 +0000

    ctrl+x

--- contracts/contracts/BORA.sol
+++ contracts/contracts/BORA.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BORA is ERC20, Ownable {
